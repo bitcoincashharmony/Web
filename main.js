62673af10fa3f31566c1010a4f46bb91ae1d2eec
bitcoincashharmony <code@bitcoincashharmony.com>
2025-01-28 17:01:30 +0800

    Add files via upload

---
+++ main.js
@@ -0,0 +1,6 @@
+// JavaScript for Bitcoin Cash Harmony website
+// Will be used for interactive elements and animations
+
+document.addEventListener('DOMContentLoaded', function() {
+    // Future interactive functionality will go here
+});
---
+++ sw.js
@@ -0,0 +1,21 @@
+// Service Worker for Bitcoin Cash Harmony website
+const CACHE_NAME = 'bch-harmony-v1';
+const ASSETS = [
+  '/',
+  '/index.html',
+  '/main.js'
+];
+
+self.addEventListener('install', (event) => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then((cache) => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (event) => {
+  event.respondWith(
+    caches.match(event.request)
+      .then((response) => response || fetch(event.request))
+  );
+});
